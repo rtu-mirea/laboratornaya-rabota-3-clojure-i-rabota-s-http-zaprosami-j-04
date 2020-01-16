0506ad9bc95bae2b346424f7385c46e721208997
Jonathan Dragosh <dragoshs.j@yahoo.com>
2020-01-16 20:21:42 +0300

    Добавлены решения задач

--- src/metrics_server/api/files.clj
+++ src/metrics_server/api/files.clj
@@ -19,3 +19,45 @@
   []
   (:data (get-files-with-http-info)))
 
+; Задание 2.1
+(defn task21 [files]
+  (filter (fn [x] (not (get x :directory))) files)
+)
+
+; Задание 2.2
+(defn task22 [files]
+  (filter (fn [x] (get x :executable)) files)
+)
+
+(defn conftocfg [filename]
+  (clojure.string/replace filename #".conf" ".cfg")
+)
+
+; Задание 2.3
+(defn task23 [files]
+  (map (fn [file]
+         {
+           :name (conftocfg (get file :name))
+           :size (get file :size)
+           :changed (get file :changed)
+           :directory (get file :directory)
+           :executable (get file :executable)
+          }
+        ) files
+  )
+)
+
+; Задание 3
+(defn task3 [files]
+  (/
+   (reduce + (map (fn [file] (get file :size)) (filter (fn [x] (not (get x :directory))) files)))
+   (count (filter (fn [x] (not (get x :directory))) files))
+   )
+)
+
+(defn -main [& args]
+  (println (task21 (get-files)))
+  (println (task22 (get-files)))
+  (println (task23 (get-files)))
+  (println (task3 (get-files)))
+)
--- src/metrics_server/api/hardware.clj
+++ src/metrics_server/api/hardware.clj
@@ -19,3 +19,17 @@
   []
   (:data (get-metrics-with-http-info)))
 
+;; Задание 1.1
+(defn task11 [metrics] (filter (fn [a] (> (get a :cpuTemp) 2)) metrics))
+
+;; Задание 1.2
+(defn task12 [metrics] (/ (reduce + (map (fn [a] (get a :cpuTemp)) metrics)) (count metrics)))
+
+;; Задание 1.3
+(defn task13 [metrics] (/ (reduce + (map (fn [a] (get a :cpuLoad)) metrics)) (count metrics)))
+
+;; Вывод
+(defn -main [& args]
+  (println (task11 (get-metrics)))
+  (println (task12 (get-metrics)))
+  (println (task13 (get-metrics))))
